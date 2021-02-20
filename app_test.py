4227f7f92effeda046cbfd4ae8f075b3a2330549
igor <alvesmoreiraigor@gmail.com>
2021-02-20 11:10:29 -0300

    teste

---
+++ app_test.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+@app.route("/deploy", methods=['GET'])
+def deploy():
+    return "<h1>Testando deploy GitHub x Heroku </h1>"
+
+
+def main():
+    port = int(os.environ.get("PORT", 5555))
+    app.run(port=port)
+
+
+if __name__ == "__main__":
+    main()
