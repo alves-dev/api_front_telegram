2a0e430069a2cbccb942ba07a6fdd4e08aca5200
igor <alvesmoreiraigor@gmail.com>
2021-03-03 07:16:05 -0300

    Test new relic

--- utils/status.py
+++ utils/status.py
@@ -4,6 +4,7 @@ import logging
 
 def get_status() -> dict:
     from app import STARTED
-    status = {'status': 'Running', 'started': STARTED, 'version': settings.get('VERSION')}
+    status = {'status': 'Running', 'started': STARTED, 'version': settings.get('VERSION'),
+              'application': 'api_front_telegram'}
     logging.info(f'Get status: {status}')
     return status
