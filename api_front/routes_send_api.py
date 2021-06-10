d9561fc6b75e1660b97c114f2f2b20a7c5dab4db
igor <alvesmoreiraigor@gmail.com>
2021-06-10 19:43:18 -0300

    new version 3.1.0 => add parameter TYPE_MESSAGE_NOT_REDIRECT = ['Info']

--- api_front/routes_send_api.py
+++ api_front/routes_send_api.py
@@ -3,7 +3,7 @@ from api_telegram import bot_telegram
 import logging
 from .authentication import token_required
 from flask import Blueprint
-
+from config import settings
 
 bp_send = Blueprint('send_api', __name__, url_prefix='/send')
 
@@ -15,6 +15,8 @@ def message():
     body = request.get_json()
     logging.info(f'body: {body}')
     print(body)
+    if 'type' in body.keys() and body['type'] in settings.get('TYPE_MESSAGE_NOT_REDIRECT'):
+        return jsonify({'return_send': False, 'parameters': body, 'caused': 'Not send: TYPE_MESSAGE_NOT_REDIRECT'}), 200
     return_send = bot_telegram.send_message(body)
     if not return_send:
         return_send = bot_telegram.send_message(body)
